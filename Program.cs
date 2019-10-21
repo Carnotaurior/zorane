add590452c9f06fd19beea01f2144f1e09d07f10
Carnotaurior <56769434+Carnotaurior@users.noreply.github.com>
2019-10-21 13:10:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Banka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uzivatel uz = new Uzivatel(25,'m',"654", 65482168765321);
+            Uzivatel uzi = new Uzivatel();
+            
+            Console.WriteLine(uz.getVek());
+            Console.WriteLine(uzi.getVek());
+            Console.ReadKey();
+        }
+    }
+}
