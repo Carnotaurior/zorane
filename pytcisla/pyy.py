baf9c0592c6421b3d5650cd19cb7df6be0085718
Carnotaurior <56769434+Carnotaurior@users.noreply.github.com>
2020-12-11 09:42:33 +0100

    Add files via upload

---
+++ pytcisla/pyy.py
@@ -0,0 +1,15 @@
+def jsouPyt(x,y,z):
+    print(__name__)
+    if x**2 + y**2 == z**2:
+        print("Ano")
+    else:
+        print("Ne")
+
+if __name__ == "__main__":
+    a = int(input("zadej"))
+    b = int(input("zadej"))
+    c = int(input("zadej"))
+    jsouPyt(a,b,c)
+    print("kod")    
+    
+    
---
+++ pytcisla/trojuhelnik.py
@@ -0,0 +1,4 @@
+import pyy
+
+pyy.jsouPyt(3,4,5)
+pyy.jsouPyt(4,4,4)
